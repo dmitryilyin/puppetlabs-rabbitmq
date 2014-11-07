82518db13d914cd76d2daed74e2fa7e55957e67c
Dmitry Ilyin <dilyin@mirantis.com>
2014-11-07 22:15:33 +0300

    Rewrite rabbitmq_user

--- lib/puppet/provider/rabbitmq_common.rb
+++ lib/puppet/provider/rabbitmq_common.rb
@@ -2,6 +2,105 @@ require 'set'
 
 class Puppet::Provider::Rabbitmq_common < Puppet::Provider
 
+  # Status functions
+  ##################
+
+  # get the RabbitMQ version number
+  # @return [Float]
+  def self.version
+    return @version if @version
+    status_text = rabbitmqctl '-q', 'status'
+    status_text.split("\n").each do |line|
+      begin
+        if line =~ %r{"RabbitMQ","([\d\.]+)"}
+          version = $1.split('.')
+          @version =  "#{version[0]}.#{version[1..-1].join ''}".to_f
+          return @version
+        end
+      rescue
+        return @version = nil
+      end
+    end
+    @version = nil
+  end
+
+  # get the RabbitMQ version number
+  # resets mnemoisation
+  # @return [Float]
+  def self.version_with_renew
+    @version = nil
+    self.version
+  end
+
+  # check if this version supports tags
+  # @return [TrueClass,FalseClass]
+  def self.tag_support?
+    self.version && self.version > 2.41
+  end
+
+  # User functions
+  ################
+
+  ADMIN_TAG = 'administrator'
+
+  def self.parse_user_list_line_old(line)
+    if line =~ %r{^(\S+)\s+(true|false)}
+      user = $1.strip
+      user_structure = {
+          :tags => Set.new,
+          :admin => $2 == 'true',
+      }
+      @user_list.store user, user_structure
+    end
+    @user_list
+  end
+
+  def self.parse_user_list_line_new(line)
+    if line =~ %r{(.*?)\[(.*?)\]}
+      user = $1.strip
+      admin = false
+      tags = []
+      $2.split(',').each do |tag|
+        tag.strip!
+        if tag == ADMIN_TAG
+          admin = true
+        else
+          tags << tag
+        end
+      end
+      user_structure = {
+          :tags => Set.new tags,
+          :admin => admin,
+      }
+      @user_list.store user, user_structure
+    end
+    @user_list
+  end
+
+  # get the list of users and their tags
+  # @return [Hash<String => Set>]
+  def self.user_list
+    return @user_list unless @user_list
+    user_list_text = rabbitmqctl '-q', 'list_users'
+    @user_list = {}
+    user_list_text.split("\n").each do |line|
+      if self.tag_support?
+        self.parse_user_list_line_new line
+      else
+        self.parse_user_list_line_old line
+      end
+    end
+    user_list
+  end
+
+  # get the list of users and their tags
+  # resets mnemoisation
+  # @return [Hash<String => Set>]
+  def self.user_list_with_renew
+    @user_list = nil
+    self.user_list
+  end
+
   # Wait functions
   ################
 
--- lib/puppet/provider/rabbitmq_user/rabbitmqctl.rb
+++ lib/puppet/provider/rabbitmq_user/rabbitmqctl.rb
@@ -1,100 +1,126 @@
-require File.join File.dirname(__FILE__), '../rabbitmq_common.rb'
-
-Puppet::Type.type(:rabbitmq_user).provide(:rabbitmqctl, :parent => Puppet::Provider::Rabbitmq_common) do
+require 'set'
+Puppet::Type.type(:rabbitmq_user).provide(:rabbitmqctl) do
 
   if Puppet::PUPPETVERSION.to_f < 3
     commands :rabbitmqctl => 'rabbitmqctl'
   else
-     has_command(:rabbitmqctl, 'rabbitmqctl') do
-       environment :HOME => "/tmp"
-     end
+    has_command(:rabbitmqctl, 'rabbitmqctl') do
+      environment :HOME => "/tmp"
+    end
   end
 
   defaultfor :feature => :posix
 
+  # create provider instance for every resource found.
+  # User for resource discovery and can be used for prefetch
+  # FIXME: discovery will not work because type instances cannot be created without password
   def self.instances
-    self.wait_for_online
-    rabbitmqctl('-q', 'list_users').split(/\n/).collect do |line|
-      if line =~ /^(\S+)(\s+\[.*?\]|)$/
-        new(:name => $1)
-      else
-        raise Puppet::Error, "Cannot parse invalid user line: #{line}"
+    self.users.keys.map do |user|
+      new({ :name => user })
+    end
+  end
+
+  # take hash of type instances from catalog and try
+  # to match them with present provider if their names are same
+  # @param catalog_resources [Hash]
+  def self.prefetch(catalog_resources)
+    Puppet.debug 'Call: prefetch rabbitmq_user'
+    present_resources = self.instances
+    catalog_resources.keys.each do |resource_name|
+      found_provider = present_resources.find { |instance| instance.name == resource_name }
+      if found_provider
+        catalog_resources[resource_name].provider = found_provider
       end
     end
   end
 
+  # create this user and set it's tags
   def create
-    rabbitmqctl('add_user', resource[:name], resource[:password])
-    if resource[:admin] == :true
-      make_user_admin()
-    end
-    if !resource[:tags].nil?
-      set_user_tags(resource[:tags])
-    end
+    Puppet.debug "Call: create rabbitmq_user '#{resource[:name]}'"
+    rabbitmqctl 'add_user', resource[:name], resource[:password]
+    self.tags = resource[:tags]
   end
 
+  # delete this user
   def destroy
-    rabbitmqctl('delete_user', resource[:name])
+    Puppet.debug "Call: destroy rabbitmq_user '#{resource[:name]}'"
+    rabbitmqctl 'delete_user', resource[:name]
   end
 
+  # check if this users exists
+  # @return [TrueClass,FalseClass]
   def exists?
-    self.class.wait_for_online
-    rabbitmqctl('-q', 'list_users').split(/\n/).detect do |line|
-      line.match(/^#{Regexp.escape(resource[:name])}(\s+(\[.*?\]|\S+)|)$/)
-    end
+    Puppet.debug "Call: exists? rabbitmq_user '#{resource[:name]}'"
+    out = self.class.users.key? resource[:name]
+    Puppet.debug "Return: #{out.inspect} (#{out.class})"
+    out
   end
 
- 
+  # get an array of this user's tags
+  # reject admin tag because it's implicitly sets by admin => true
+  # @return [Array<String>]
   def tags
-    get_user_tags.entries.sort
+    Puppet.debug "Call: tags rabbitmq_user '#{resource[:name]}'"
+    out = get_user_tags.entries.reject { |tag| tag == ADMIN_TAG }.sort
+    Puppet.debug "Return: #{out.inspect} (#{out.class})"
+    out
   end
 
-
+  # set tags for this user
+  # implicitly add admin tag if the user is admin
+  # @param tags [Array<String>]
   def tags=(tags)
-    if ! tags.nil?
-      set_user_tags(tags)
+    Puppet.debug "Call: tags= rabbitmq_user '#{resource[:name]}' with '#{tags.inspect} (#{tags.class})'"
+    user_tags = Set.new tags
+    user_tags.add ADMIN_TAG if get_user_tags.member?(ADMIN_TAG) || (resource[:admin] == :true)
+    if self.class.tag_support?
+      rabbitmqctl 'set_user_tags', resource[:name], user_tags.entries.sort
+    else
+      set_unset_admin user_tags
     end
   end
 
+  # get information if this user is set to be administrator
+  # by checking if administrator tag is present
+  # @return [:true,:false]
   def admin
-    if usertags = get_user_tags
-      (:true if usertags.include?('administrator')) || :false
-    else
-      raise Puppet::Error, "Could not match line '#{resource[:name]} (true|false)' from list_users (perhaps you are running on an older version of rabbitmq that does not support admin users?)"
-    end
+    Puppet.debug "Call: admin rabbitmq_user '#{resource[:name]}'"
+    out = self.class.users.fetch(resource[:name], []).include? ADMIN_TAG
+    out = out.to_s.to_sym
+    Puppet.debug "Return: #{out.inspect} (#{out.class})"
+    out
   end
 
+  # set this user to be the administrator
+  # by adding admin tag or removing the tag to remove the admin status
+  # @param state [:true,:false]
   def admin=(state)
+    user_tags = get_user_tags
     if state == :true
-      make_user_admin()
+      user_tags.add ADMIN_TAG
     else
-      usertags = get_user_tags
-      usertags.delete('administrator')
-      rabbitmqctl('set_user_tags', resource[:name], usertags.entries.sort)
+      user_tags.delete ADMIN_TAG
+    end
+    if self.class.tag_support?
+      rabbitmqctl 'set_user_tags', resource[:name], user_tags.entries.sort
+    else
+      set_unset_admin user_tags
     end
   end
 
-  def set_user_tags(tags)
-    is_admin = get_user_tags().member?("administrator") \
-               || resource[:admin] == :true
-    usertags = Set.new(tags)
-    if is_admin
-      usertags.add("administrator")
+  # old admin control method support
+  # @param tags [Set<String>]
+  def set_unset_admin(tags)
+    if tags.include? ADMIN_TAG
+      rabbitmqctl 'set_admin', resource[:name]
+    else
+      rabbitmqctl 'clear_admin', resource[:name]
     end
-    rabbitmqctl('set_user_tags', resource[:name], usertags.entries.sort)
   end
 
-  def make_user_admin
-    usertags = get_user_tags
-    usertags.add('administrator')
-    rabbitmqctl('set_user_tags', resource[:name], usertags.entries.sort)
-  end
-
-  private
+  # get a set of tags of this user
+  # @return [Set<String>]
   def get_user_tags
-    match = rabbitmqctl('-q', 'list_users').split(/\n/).collect do |line|
-      line.match(/^#{Regexp.escape(resource[:name])}\s+\[(.*?)\]/)
-    end.compact.first
-    Set.new(match[1].split(/, /)) if match
+    self.class.users.fetch resource[:name], Set.new
   end
 end
